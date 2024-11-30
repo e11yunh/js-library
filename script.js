a91b06426a8e8f4d5dfad13a24fb44a0ee3809dc
Elliot Yun <e11yunh@gmail.com>
2024-11-30 13:39:30 +0800

    implement basic card slot layout for read books

--- script.js
+++ script.js
@@ -12,12 +12,21 @@ function addBookToLibrary(selected_book) {
 }
 
 
-console.log(myLibrary)
-
 book1 = new Book("James Hurt", "Leslie ing", 172, true)
 book2 = new Book("Ironic Star", "Love is Terrible", 999, false)
 
 addBookToLibrary(book1)
 addBookToLibrary(book2)
 
-console.log(myLibrary)
+
+const read_books = document.querySelectorAll(".read");
+
+read_books.forEach(book => {
+    const btnContainer = book.nextElementSibling;
+
+    const readButton = btnContainer.children[0];
+    readButton.textContent = "Unread";
+    readButton.style.backgroundColor = "hsl(81, 40%, 70%)";
+
+    book.closest(".card").style.boxShadow = 'inset 0 9px 0 hsl(81, 88%, 80%), 2px 3px 2px hsla(0, 0%, 0%, 0.45)';
+});
